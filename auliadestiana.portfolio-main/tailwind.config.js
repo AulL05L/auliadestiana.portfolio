7b3740767c7329bd551402cffb3562d9317c83a6
AulL05L <155145751+AulL05L@users.noreply.github.com>
2025-06-12 20:23:44 +0700

    Add files via upload

---
+++ auliadestiana.portfolio-main/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/index.html"],
+  theme: {
+    extend: {
+      colors: {
+        cream: "#bfa891",
+        dove: "#e0d1bf",
+        moss: "#9a927b",
+        jungle: "#3d3a30",
+        terraC: "#803407",
+        charcoal: "#141414",
+      },
+    },
+  },
+  plugins: [],
+};
